3b292f83924004df15828dff5dd04d7be7c12a76
Mason Scott <mscott2757@gmail.com>
2024-06-23 00:07:07 -0700

    progress

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import './styles/scss/index.scss';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+// import registerServiceWorker from './registerServiceWorker';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
 root.render(<App />);
-registerServiceWorker();
+// registerServiceWorker();
